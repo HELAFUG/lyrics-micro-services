72f73360dc11e736b193f2f7825d490df81c1f76
HELAFUG <loxand42@gmail.com>
2025-12-13 11:14:47 +0300

    add database helper

---
+++ backend/auth-service/app/core/models/db_helper.py
@@ -0,0 +1,31 @@
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from core.config import settings
+
+
+class DBHelper:
+    def __init__(self, db_url: str, echo: bool):
+        self.engine = create_async_engine(
+            url=db_url,
+            echo=echo,
+        )
+
+        self.session_factory = async_sessionmaker(
+            bind=self.engine,
+            expire_on_commit=False,
+            autoflush=False,
+            autocommit=False,
+        )
+
+        async def session_getter() -> AsyncGenerator[AsyncSession, None]:
+            async with self.session_factory() as session:
+                yield session
+
+        async def dispose(self):
+            await self.engine.dispose()
+
+
+db_helper = DBHelper(
+    db_url=settings.db.url,
+    echo=settings.db.echo,
+)

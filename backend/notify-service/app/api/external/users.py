8aa5e45ce5c9b94dad6fb5d579b91cc655609cc5
HELAFUG <loxand42@gmail.com>
2025-12-14 14:01:36 +0300

    add email validator

--- backend/notify-service/app/api/external/users.py
+++ backend/notify-service/app/api/external/users.py
@@ -1,3 +1,4 @@
+from pydantic import EmailStr
 from fastapi import APIRouter, Response
 from mailing.welcome import send_welcome_email, send_login_email
 
@@ -7,12 +8,12 @@ users_router = APIRouter(
 
 
 @users_router.post("/after-register/{user_email}")
-async def after_register(user_email: str):
+async def after_register(user_email: EmailStr):
     await send_welcome_email(email=user_email)
     return Response(status_code=200)
 
 
 @users_router.post("/after-login/{user_email}")
-async def after_login(user_email: str):
+async def after_login(user_email: EmailStr):
     await send_login_email(email=user_email)
     return Response(status_code=200)

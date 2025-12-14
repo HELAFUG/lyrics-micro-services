1843dda1641202e490db2fb6f521269280372a95
HELAFUG <loxand42@gmail.com>
2025-12-14 13:53:15 +0300

    add logging in welcome email

--- backend/notify-service/app/mailing/welcome/welcome.py
+++ backend/notify-service/app/mailing/welcome/welcome.py
@@ -1,5 +1,9 @@
+import logging
 from mailing.welcome.send_mail import send_email
+
+log = logging.getLogger(__name__)
 
 
 async def send_welcome_email(email: str) -> None:
+    log.info("Sending welcome email to %s", email)
     await send_email(email, "Welcome to Lyrics", "Thanks for registering")

add6d66f7bda50d9688006c9526ed677f07d09ec
HELAFUG <loxand42@gmail.com>
2025-12-15 22:28:07 +0300

    add after login func

--- backend/auth-service/app/service/http/notify_service.py
+++ backend/auth-service/app/service/http/notify_service.py
@@ -8,7 +8,22 @@ log = logging.getLogger(__name__)
 async def after_register(user_email: str):
     async with ClientSession(base_url=settings.notify_service.url) as session:
         async with session.post(
-            f"{settings.notify_service.after_register_url}"
+            f"{settings.notify_service.after_register_url}/{user_email}"
+        ) as response:
+            match response.status:
+                case 200:
+                    log.info("Email sent to %s", user_email)
+                case 422:
+                    log.error("Email is not correct %s", user_email)
+
+                case _:
+                    log.error("Error sending email to %s", user_email)
+
+
+async def after_login(user_email: str):
+    async with ClientSession(base_url=settings.notify_service.url) as session:
+        async with session.post(
+            f"{settings.notify_service.after_login_url}/{user_email}"
         ) as response:
             match response.status:
                 case 200:

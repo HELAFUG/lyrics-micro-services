fa19f3d518f2764743d674a1ccd6d7e6dfe88353
HELAFUG <loxand42@gmail.com>
2025-12-16 17:52:50 +0300

    add settings to main

--- backend/music-service/app/main.py
+++ backend/music-service/app/main.py
@@ -1,9 +1,23 @@
 import uvicorn
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from core.config import settings
+from api import api_router
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(api_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8060)
+    uvicorn.run(
+        "main:app",
+        host=settings.srv.host,
+        port=settings.srv.port,
+        reload=settings.srv.reload_on_change,
+    )

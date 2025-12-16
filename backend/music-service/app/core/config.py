806c5e120667d131eb0fe8c290add25b84f8f003
HELAFUG <loxand42@gmail.com>
2025-12-16 18:42:05 +0300

    add env lib

--- backend/music-service/app/core/config.py
+++ backend/music-service/app/core/config.py
@@ -1,6 +1,9 @@
 from os import getenv
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class AuthServiceConfig(BaseSettings):

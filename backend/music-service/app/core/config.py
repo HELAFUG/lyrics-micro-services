9dec34cfbe763bfb9bd50d8c4062a9298582d68a
HELAFUG <loxand42@gmail.com>
2025-12-16 15:45:41 +0300

    add config and first struct

---
+++ backend/music-service/app/core/config.py
@@ -0,0 +1,11 @@
+from os import getenv
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+
+class AuthServiceConfig(BaseSettings):
+    auth_service_url: str = "http://localhost:8040"
+
+    @property
+    def check_user_exist(self):
+        return f"{self.auth_service_url}/service-api/check_user_exist"

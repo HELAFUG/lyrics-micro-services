dabb7404cee586bbd4d4fcd186c1f75161bfab6e
HELAFUG <loxand42@gmail.com>
2025-12-16 17:38:52 +0300

    create a normal config

--- backend/music-service/app/core/config.py
+++ backend/music-service/app/core/config.py
@@ -9,3 +9,34 @@ class AuthServiceConfig(BaseSettings):
     @property
     def check_user_exist(self):
         return f"{self.auth_service_url}/service-api/users/check_user_exist"
+
+
+class ExternalServices(BaseModel):
+    auth: AuthServiceConfig = AuthServiceConfig()
+
+
+class APIV1(BaseModel):
+    prefix: str = "/v1"
+    external: str = "/external"
+    profiles: str = "/profiles"
+    tracks: str = "/tracks"
+
+
+class APIConfig(BaseModel):
+    prefix: str = "/api"
+    v1: APIV1 = APIV1()
+
+
+class SRVSettings(BaseModel):
+    host: str = "0.0.0.0"
+    port: int = 8060
+    reload_on_change: bool = True
+
+
+class Settings(BaseModel):
+    api: APIConfig = APIConfig()
+    srv: SRVSettings = SRVSettings()
+    external: ExternalServices = ExternalServices()
+
+
+settings = Settings()

3be9f841342f9c93c774a374f1fa1c2e05615208
HELAFUG <loxand42@gmail.com>
2025-12-16 16:04:37 +0300

    reload in general

--- backend/auth-service/app/api/external/users.py
+++ backend/auth-service/app/api/external/users.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
 from pydantic import EmailStr
+from sqlalchemy.ext.asyncio import AsyncSession
 from service.users import check_exist_user
+from core.models import db_helper
 
 "service-api/check_user_exist"
 
@@ -8,5 +11,8 @@ users_router = APIRouter(prefix="/users")
 
 
 @users_router.get("/check_exist_user/{email}")
-async def check_user_in_db(email: EmailStr):
-    return await check_exist_user(email=email)
+async def check_user_in_db(
+    email: EmailStr,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    return await check_exist_user(session=session, email=email)
--- backend/auth-service/app/service/users/services.py
+++ backend/auth-service/app/service/users/services.py
@@ -1,10 +1,11 @@
+from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import HTTPException
 from repository.user import get_user_by_email
 
 
-async def check_exist_user(email: str):
-    user = await get_user_by_email(email=email)
+async def check_exist_user(session: AsyncSession, email: str):
+    user = await get_user_by_email(session=session, email=email)
     if user:
-        raise HTTPException(status_code=200, detail={"user_exist_in_db": True})
+        raise HTTPException(status_code=200, detail=True)
 
-    return {"user_exist_in_db": False}
+    return False

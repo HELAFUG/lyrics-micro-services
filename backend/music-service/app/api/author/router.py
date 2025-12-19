3c6710ed33c2965813c0b47de7083b5646593c6d
HELAFUG <loxand42@gmail.com>
2025-12-19 15:51:17 +0300

    add some new details

--- backend/music-service/app/api/author/router.py
+++ backend/music-service/app/api/author/router.py
@@ -1,10 +1,16 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 from core.schemas.author import AuthorCreate
-from service.auth_external import fetch_exist_user
+from core.models import db_helper
+from service.author import create_new_author
 
 router = APIRouter()
 
 
 @router.post("/new-author")
-async def new_author(author: AuthorCreate):
-    pass
+async def new_author(
+    author: AuthorCreate,
+    session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
+):
+    return await create_new_author(session=session, author_in=author)
--- backend/music-service/app/service/auth_external/service.py
+++ backend/music-service/app/service/auth_external/service.py
@@ -1,4 +1,5 @@
 from aiohttp import ClientSession
+from aiohttp.client_exceptions import ClientConnectorError
 from core.config import settings
 from fastapi import HTTPException
 
@@ -8,10 +9,16 @@ async def fetch_exist_user(email: str) -> bool:
         async with session.get(
             f"{settings.external.auth.check_user_exist}/{email}"
         ) as response:
-            resp_dict = await response.json()
-            if response.status != 200:
+            try:
+                resp_dict = await response.json()
+                if response.status != 200:
+                    raise HTTPException(
+                        status_code=response.status,
+                        detail=resp_dict.get("detail", "Unknown error"),
+                    )
+
+            except ClientConnectorError as e:
                 raise HTTPException(
-                    status_code=response.status,
-                    detail=resp_dict.get("detail", "Unknown error"),
+                    status_code=400, detail=str("External service error")
                 )
             return resp_dict.get("detail", False)

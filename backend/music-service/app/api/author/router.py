4a1d3ad5d6c949a269a9a60308c9ac20e0046bcd
HELAFUG <loxand42@gmail.com>
2025-12-19 16:02:51 +0300

    add exeption for reason

--- backend/music-service/app/api/author/router.py
+++ backend/music-service/app/api/author/router.py
@@ -1,5 +1,5 @@
 from typing import Annotated
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.schemas.author import AuthorCreate
 from core.models import db_helper
@@ -13,4 +13,9 @@ async def new_author(
     author: AuthorCreate,
     session: Annotated[AsyncSession, Depends(db_helper.session_getter)],
 ):
-    return await create_new_author(session=session, author_in=author)
+    result = await create_new_author(session=session, author_in=author)
+    if result:
+        return result
+    raise HTTPException(
+        status_code=400, detail=f"User with email {author.email} already exist"
+    )
